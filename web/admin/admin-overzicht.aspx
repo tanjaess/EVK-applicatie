6e20695e57863a199b8da6fd443686c06cb5361d
tanjaess <tanja.esselens@gmail.com>
2011-11-28 00:15:08 +0100

    readme

---
+++ web/admin/admin-overzicht.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/layout/master.master" AutoEventWireup="true" CodeFile="admin-overzicht.aspx.cs" Inherits="web_admin_admin" %>
+
+<asp:Content ID="head" ContentPlaceHolderID="head" Runat="Server">
+    <title>Admin</title>
+</asp:Content>
+<asp:Content ID="top" ContentPlaceHolderID="top" Runat="Server">
+    <asp:HyperLink ID="logout" runat="server">Logout Els Van Orlé</asp:HyperLink>
+    <h1>Admin</h1>
+</asp:Content>
+<asp:Content ID="customNavigation" ContentPlaceHolderID="customNavigation" Runat="Server">
+    <asp:HyperLink ID="HyperLink1" runat="server">2009</asp:HyperLink>
+    &nbsp;
+    <asp:HyperLink ID="HyperLink2" runat="server">20010</asp:HyperLink>
+</asp:Content>
+<asp:Content ID="header" ContentPlaceHolderID="header" Runat="Server">
+    <h1>Overzicht</h1>
+</asp:Content>
+<asp:Content ID="body" ContentPlaceHolderID="body" Runat="Server">
+</asp:Content>
+<asp:Content ID="footer" ContentPlaceHolderID="footer" Runat="Server">
+</asp:Content>
+
---
+++ web/admin/admin-overzicht.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class web_admin_admin : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Response.Write("<h2>Rapporten goed te keuren</h2>");
+        Table tabel = new Table();
+        tabel.BorderWidth = 1;
+        tabel.GridLines = new GridLines();
+        Page.Controls.Add(tabel);
+        string[] words = new string[3] { "Naam", "Jaar", "Status"};
+        int rows = 5;
+        int cols = 3;
+        for (int i = 0; i < words.Length; i++)
+        {
+            TableRow tr = new TableRow();
+            tr.Style.Add("border", "1px #000 solid");
+            if (i == 0)
+            {
+                tr.Style.Add("font-weight", "bold");
+
+            }
+            for (int j = 1; j <= cols; j++)
+            {
+                TableCell td = new TableCell();
+                if (i == 0)
+                    td.Text = words[j];
+                tr.Controls.Add(td);
+            }
+            tabel.Controls.Add(tr);
+        }
+    }
+}
