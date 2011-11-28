a147ff614b3dbb0ba6b59ef78ce34e90fbb9e103
tanjaess <tanja.esselens@gmail.com>
2011-11-28 14:23:23 +0100

    admin-overzicht fontend geupdate

--- web/admin/admin-overzicht.aspx
+++ web/admin/admin-overzicht.aspx
@@ -7,15 +7,11 @@
     <asp:HyperLink ID="logout" runat="server">Logout Els Van Orlé</asp:HyperLink>
     <h1>Admin</h1>
 </asp:Content>
-<asp:Content ID="customNavigation" ContentPlaceHolderID="customNavigation" Runat="Server">
-    <asp:HyperLink ID="HyperLink1" runat="server">2009</asp:HyperLink>
-    &nbsp;
-    <asp:HyperLink ID="HyperLink2" runat="server">20010</asp:HyperLink>
-</asp:Content>
 <asp:Content ID="header" ContentPlaceHolderID="header" Runat="Server">
     <h1>Overzicht</h1>
 </asp:Content>
 <asp:Content ID="body" ContentPlaceHolderID="body" Runat="Server">
+
 </asp:Content>
 <asp:Content ID="footer" ContentPlaceHolderID="footer" Runat="Server">
 </asp:Content>
--- web/admin/admin-overzicht.aspx.cs
+++ web/admin/admin-overzicht.aspx.cs
@@ -7,17 +7,19 @@ using System.Web.UI.WebControls;
 
 public partial class web_admin_admin : System.Web.UI.Page
 {
-    protected void Page_Load(object sender, EventArgs e)
+    public void drawTable(string strTitle, Int16 intRows, string[] arWords) 
     {
-        Response.Write("<h2>Rapporten goed te keuren</h2>");
+        Label lblTitleRpt = new Label();
+        lblTitleRpt.Text = strTitle;
+        Page.Controls.Add(lblTitleRpt);
         Table tabel = new Table();
         tabel.BorderWidth = 1;
         tabel.GridLines = new GridLines();
         Page.Controls.Add(tabel);
-        string[] words = new string[3] { "Naam", "Jaar", "Status"};
-        int rows = 5;
-        int cols = 3;
-        for (int i = 0; i < words.Length; i++)
+        string[] words = arWords;
+        int rows = intRows+1; // aantal records + 1 (header)
+        int cols = arWords.Length;
+        for (int i = 0; i < rows; i++)
         {
             TableRow tr = new TableRow();
             tr.Style.Add("border", "1px #000 solid");
@@ -30,10 +32,40 @@ public partial class web_admin_admin : System.Web.UI.Page
             {
                 TableCell td = new TableCell();
                 if (i == 0)
-                    td.Text = words[j];
+                {
+                    td.Text = words[j-1];
+                }
+                else
+                {
+                    switch (j)
+                    {
+                        case 1:
+                            td.Text = "naam, voornaam";
+                            break;
+                        case 2:
+                            td.Text = "naam, voornaam";
+                            break;
+                        case 3:
+                            td.Text = "nagekeken";
+                            break;
+                        case 4:
+                            td.Text = "wachtend";
+                            break;
+                    }
+                }
                 tr.Controls.Add(td);
             }
             tabel.Controls.Add(tr);
         }
+    }
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        // Raporten
+        drawTable("Overzicht rapporten", 5, new string[3] { "Student", "Jaar", "Status"});
+        // Actieve Dossiers
+        drawTable("Dossiers in behandeling", 2, new string[4] { "Student", "Vak", "Lectoren","Status"});
+        // Afgerond
+        drawTable("Overzicht afgeronde dossiers", 2, new string[2] { "Student", "Status" });
+
     }
 }
